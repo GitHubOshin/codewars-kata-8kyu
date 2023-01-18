283ba6ef37a97b3571f69c917ab395bd3f2cc96f
OSHIN <81525447+GitHubOshin@users.noreply.github.com>
2023-01-18 20:44:11 +0700

    str repeat

---
+++ 1-50/11-string-repeat.js
@@ -0,0 +1,24 @@
+/**
+ * String repeat
+ * https://www.codewars.com/kata/57a0e5c372292dd76d000d7e
+ *
+ * FUNDAMENTALS  |  STRINGS
+ *
+ * DESCRIPTION:
+ *
+ *   Write a function that accepts an integer n and a string s as parameters,
+ *   and returns a string of s repeated exactly n times.
+ *
+ * Examples (input -> output)
+ *
+ *   6, "I"     -> "IIIIII"
+ *   5, "Hello" -> "HelloHelloHelloHelloHello"
+ */
+
+function repeatStr(n = 0, s = '') {
+  return s.repeat(n)
+}
+
+console.log(repeatStr(3, '*')) // "***"
+console.log(repeatStr(5, '#')) // "#####"
+console.log(repeatStr(2, 'ha ')) // "ha ha "
